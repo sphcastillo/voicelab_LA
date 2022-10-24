a841d7a60adc378af391fe53809de1d43bd23f67
Sophia Castillo <Sphcastillo@gmail.com>
2022-10-24 11:55:59 -0700

    added RegisterScreen / LoginScreen, implementing Firebase with Error

--- App.js
+++ App.js
@@ -1,12 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import  { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
+import HomeScreen from './screens/HomeScreen';
+
+const Stack = createStackNavigator();
+
+const globalScreenOptions = {
+  headerStyle: { backgroundColor: "#FA8072" },
+  headerTitleStyle: { color: "white" },
+  headerTintColor: "white",
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>VoiceLab LA</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={globalScreenOptions}>
+        <Stack.Screen 
+          name="Login"
+          component={LoginScreen}
+        />
+        <Stack.Screen 
+          name="Register"
+          component={RegisterScreen}
+        />
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+
   );
 }
 
