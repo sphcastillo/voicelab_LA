cb1fc3638f8d50866cd5ee835ea2163c007c2ade
Sean Patrick Parnell <seanpatrickparnell@gmail.com>
2022-11-01 23:40:27 +0100

    firebase config.js keys

--- config.js
+++ config.js
@@ -12,12 +12,13 @@ import 'firebase/compat/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCtM37O204eM9_kJxj2b_sTZ-e6jEOl0bk",
-  authDomain: "voicelab-la.firebaseapp.com",
-  projectId: "voicelab-la",
-  storageBucket: "voicelab-la.appspot.com",
-  messagingSenderId: "157093903098",
-  appId: "1:157093903098:web:223e6c3819bf0b8e2a2743"
+  apiKey: "AIzaSyD61nC6omOiMWIeyPSIUwyJ3Q3HePmjH2I",
+  authDomain: "voicelab-la-3a29d.firebaseapp.com",
+  projectId: "voicelab-la-3a29d",
+  storageBucket: "voicelab-la-3a29d.appspot.com",
+  messagingSenderId: "746927898780",
+  appId: "1:746927898780:web:a365890f8e0b9004e4ed59",
+  measurementId: "G-3ETR90J9CR"
 };
 
 if (!firebase.apps.length){
